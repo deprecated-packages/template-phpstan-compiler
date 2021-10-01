99771a30282d94dee7bf9c86ca1d3ffcb76c9b53
GitHub <action@github.com>
2021-10-01 14:49:22 +0000

    [TwigPHPStanRule] add PhpToTemplateLinesNodeVisitor (#3657)

---
+++ src/Reporting/TemplateErrorsFactory.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\TemplatePHPStanCompiler\Reporting;
+
+use PHPStan\Rules\RuleError;
+use PHPStan\Rules\RuleErrorBuilder;
+use Symplify\TemplatePHPStanCompiler\ValueObject\PhpFileContentsWithLineMap;
+
+final class TemplateErrorsFactory
+{
+    /**
+     * @return RuleError[]
+     */
+    public function createErrors(
+        array $errors,
+        string $resolvedTemplateFilePath,
+        PhpFileContentsWithLineMap $phpFileContentsWithLineMap
+    ): array {
+        $ruleErrors = [];
+
+        $phpToTemplateLines = $phpFileContentsWithLineMap->getPhpToTemplateLines();
+
+        foreach ($errors as $error) {
+            // correct error PHP line number to Latte line number
+            $errorLine = (int) $error->getLine();
+            $errorLine = $this->resolveNearestPhpLine($phpToTemplateLines, $errorLine);
+
+            $ruleErrors[] = RuleErrorBuilder::message($error->getMessage())
+                ->file($resolvedTemplateFilePath)
+                ->line($errorLine)
+                ->build();
+        }
+
+        return $ruleErrors;
+    }
+
+    /**
+     * @param array<int, int> $phpToTemplateLines
+     */
+    private function resolveNearestPhpLine(array $phpToTemplateLines, int $desiredLine): int
+    {
+        $lastTemplateLine = 1;
+
+        foreach ($phpToTemplateLines as $phpLine => $templateLine) {
+            if ($desiredLine > $phpLine) {
+                $lastTemplateLine = $templateLine;
+                continue;
+            }
+
+            // find nearest neighbor - in case of multiline PHP replacement per one latte line
+            return $templateLine;
+        }
+
+        return $lastTemplateLine;
+    }
+}
